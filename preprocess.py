d4a8fd9808d40be4f310f41a481e5b803937cfcb
Raghav Gupta <raghav0296@gmail.com>
2017-04-12 01:08:05 +0530

    Add files via upload

--- preprocess.py
+++ preprocess.py
@@ -21,30 +21,34 @@ d = np.reshape(d,(d.shape[0]*d.shape[1]))
 d = np.sort(d)
 l1 = int(len(d)/2)
 m1 = d[l1]
-print("Q1 ",m1)
+print("median ",m1)
 d1,d2 = d[:l1],d[l1:]
 l2 = int(len(d1)/2)
 m2 = d[l2]
-print("Q2 ",m2)
+print("lower quartile ",m2)
 l3 = int(len(d2)/2)
 m3 = d[l1+l3]
-print("Q3 ",m3)
+print("upper quartile ",m3)
 iqr = m3-m2
 outlierLim = (m3+1.5*iqr)
 print("maximum ",outlierLim)
 outlierLimMin = m2-1.5*iqr
 print("minimum ",outlierLimMin)
 
-# For LSTM - remove 0s and outliers
+# For LSTM - replace 0s by second most min and outliers by max
 d2 = []
 D = newD[:,3:]
 D = np.reshape(D,(D.shape[0]*D.shape[1]))
-print (len(D))
+min2 = sorted(set(D))[1]
+# print(min2) # =1
 for i in range(len(D)):
-    if D[i]!=0 and D[i]<outlierLim:
+    if D[i]==0:
+        d2.append(min2)
+    elif D[i]>outlierLim:
+        d2.append(outlierLim)
+    else:
         d2.append(D[i])
 d2 = np.array(d2)
-print(d2)
 
 g = open("oneArrayPowers",'wb')
 pickle.dump(d2,g)
