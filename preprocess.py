57011c7dbcaa17ca7a8b3b664baad14a20853c61
Raghav Gupta <raghav0296@gmail.com>
2017-04-12 00:22:00 +0530

    Add files via upload

--- preprocess.py
+++ preprocess.py
@@ -21,12 +21,19 @@ d = np.reshape(d,(d.shape[0]*d.shape[1]))
 d = np.sort(d)
 l1 = int(len(d)/2)
 m1 = d[l1]
+print("Q1 ",m1)
 d1,d2 = d[:l1],d[l1:]
 l2 = int(len(d1)/2)
 m2 = d[l2]
+print("Q2 ",m2)
 l3 = int(len(d2)/2)
 m3 = d[l1+l3]
-outlierLim = (m3*1.5)
+print("Q3 ",m3)
+iqr = m3-m2
+outlierLim = (m3+1.5*iqr)
+print("maximum ",outlierLim)
+outlierLimMin = m2-1.5*iqr
+print("minimum ",outlierLimMin)
 
 # For LSTM - remove 0s and outliers
 d2 = []
@@ -37,7 +44,7 @@ for i in range(len(D)):
     if D[i]!=0 and D[i]<outlierLim:
         d2.append(D[i])
 d2 = np.array(d2)
-# print(d2)
+print(d2)
 
 g = open("oneArrayPowers",'wb')
 pickle.dump(d2,g)
